dcdf3790b023cd478493b397fe0ad190951df081
Alexis Lopez <ale_sl10@live.com.ar>
2023-07-29 20:41:11 -0300

    probando 2

--- src/components/buttonProducts/index.jsx
+++ src/components/buttonProducts/index.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom"
 
-const cerrar =document.querySelector('#cerrar')
-const nav =document.querySelector('#nav')
+
 
 const Abrir = (onClick) =>{
+    const cerrar =document.querySelector('#cerrar')
+    const nav =document.querySelector('#nav')
+
     nav.classList.remove('hidden')
     cerrar.classList.remove('hidden')
 }
 
 const Cerrar = (onClick) => {
-    
+    const cerrar =document.querySelector('#cerrar')
+    const nav =document.querySelector('#nav')
+
     nav.classList.add('hidden')
+    cerrar.classList.add('hidden')
 }
 
 
@@ -21,9 +26,8 @@ function Filter({ categories, filterCategoriy }) {
                 <div className="flex flex-no-shrink items-stretch h-12">
                     <Link to={'/'} className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">Inicio</Link>
                     <button className="block lg:hidden cursor-pointer ml-auto relative w-12 h-12 p-4">
-                    
-                    <img src="./images/iconos/hombre (1).png" onClick={() =>Abrir()} className="fill-current text-white"  alt=""/>
-                        
+                        <svg onClick={() =>Abrir()} className="fill-current text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+                        <svg id="cerrar" onClick={()=>Cerrar()} className="hidden fill-current text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" /></svg>
                     </button>
                 </div>
                 <div id="nav" class="hidden lg:flex lg:items-stretch lg:flex-no-shrink lg:flex-grow">
