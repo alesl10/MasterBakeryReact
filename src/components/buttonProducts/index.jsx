baa0b8f3f6843e5cb8c6ac7ca08251bd6f070965
Alexis Lopez <ale_sl10@live.com.ar>
2023-07-29 20:29:42 -0300

    prueba menu hamburguesa

--- src/components/buttonProducts/index.jsx
+++ src/components/buttonProducts/index.jsx
@@ -21,8 +21,9 @@ function Filter({ categories, filterCategoriy }) {
                 <div className="flex flex-no-shrink items-stretch h-12">
                     <Link to={'/'} className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">Inicio</Link>
                     <button className="block lg:hidden cursor-pointer ml-auto relative w-12 h-12 p-4">
-                        <svg id="abrir" onClick={() =>Abrir()} className="fill-current text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-                        <svg id="cerrar" onClick={()=>Cerrar()} className="hidden fill-current text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" /></svg>
+                    
+                    <img src="./images/iconos/hombre (1).png" onClick={() =>Abrir()} className="fill-current text-white"  alt=""/>
+                        
                     </button>
                 </div>
                 <div id="nav" class="hidden lg:flex lg:items-stretch lg:flex-no-shrink lg:flex-grow">
